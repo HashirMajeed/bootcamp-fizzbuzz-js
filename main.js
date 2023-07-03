c1d666a8a2a22fd11a8d0eba731d915a7b201aa3
Hashir Majeed <hashir.majeed@softwire.com>
2023-07-03 14:40:44 +0100

    part one: fizz buzz

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ function fizzbuzz() {
     console.log("Hello, world!")
 
     // Code goes here...
+    for (let i = 1; i <= 100; i++) {
+        var result = "";
+        if (i % 3 == 0) {
+            result += "Fizz";
+        }
+        if (i % 5 == 0) {
+            result += "Buzz";
+        }
+        if (result == "") {
+            result = i;
+        }
+
+        console.log(result);
+    }
 }
 
 // Call main function
